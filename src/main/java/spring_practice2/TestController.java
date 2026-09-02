ffcbc6d2546ccf251f817de0fdef4ae31644e084
seokam36 <seokam36@gmail.com>
2026-09-02 17:29:58 +0900

    spring_practice2

--- src/main/java/spring_practice2/TestController.java
+++ src/main/java/spring_practice2/TestController.java
@@ -1,6 +1,7 @@
 package spring_practice2;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
