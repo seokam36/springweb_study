56d8417fe88d08321d1925266b823106e86e3211
seokam36 <seokam36@gmail.com>
2026-09-11 10:34:03 +0900

    js

---
+++ src/main/resources/static/day08/index.js
@@ -0,0 +1,97 @@
+let count = 2
+// 2. 문자열 템프릿 , `백틱
+console.log(`hello ${count}`)
+let html = `<div> hello ${count} </div>`
+console.log(html)
+
+// 3.1 조건문 if
+const point = 85;
+// 3.2 삼항연산자 , 조건 ? 참 : 거짓
+// 3.3 단축평가 , 조건 && 참이면 결과 , 조건 || 거짓이면 결과
+console.log(point>=90 && 'A학점') // 90이상이면 A학점 아니면 false
+console.log(point>=90 || 'A학점') // 90점이상이면 true 아니면 A학점
+
+// 3.4 null 체크연산자 , 조건 ?? null이면 결과
+const nickName = null;
+console.log(nickName ?? '익명사용자')
+
+// 4. 반복문
+const array = [10, 20, 30, 40, 50]
+// 4.1 일반 for문
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
+// 4.2 향상된 for문
+// in 인덱스 하나씩 순회
+for (let index in array) {
+    console.log(array[index])
+}
+// of 값 하나씩 순회
+for (const index of array) {
+    console.log(index)
+}
+
+// 4.3 forEach 순회 , map 순회+반환 , filter 순회+조건(논리)
+array.forEach(value => {console.log(value)}) // js에서는 => , 자바에서는 ->
+let newArray = array.map(value => {return value}); // forEach는 반환 X, map은 반환 o
+let newArray2 = array.filter(value => {return value >= 20}); // filter 조건에 따른 반환
+
+// 5. 함수
+// 5.1 선언적 함수
+function func1(a,b){}
+
+// 5.2 익명함수 , 주로 변수,상수에 저장
+const func2 = function(a,b){}
+
+// 5.3 화살표/람다 함수 , 주로 변수,상수에 저장
+const func3 = (a,b)=>{}
+
+// 5.4 매개변수에 기본값 대입가능 , 만일 인수가 없을 때 기본값 대입
+const func4 = (a,b,c = 'student') => {}
+
+// ----- 함수 호출 ------
+func1(4,10)
+func2(4,10) // 익명, 람다함수는 변수명 호출
+func3(1, {name : 'd'})
+func4(1,2) // 만일 인수가 없는 배개변수는 기본값 대입
+
+// 6. 객체 : 여러개 값을 가진 하나의 값
+// 6.1 속성명(key) : 속성값(value) , 값에는 객체/배열/함수도 저장 가능
+const obj1 = {name:'유재석', age:10, func1:(param)=>{}}
+console.log(obj1.func1())       // 속성명으로 호출
+
+// 6.2 [값, 값, 값]
+const obj2 = ['ㅇㅇ', 1 , (param)=>{}]
+console.log(obj2[2]())          // 인덱스로 호출
+
+// 7. 스프레드 연산자 : ...배열 또는 객체 복사 , 주소값 변경 목적(리액트/플러터)
+const obj3 = {...obj1, phone:'010'} // {...기존객체, 새로운 속성}
+console.log(obj3)
+const obj4 = ['010', ...obj2] // [...기본배열, 새로운값]
+console.log(obj4)
+
+// 8. 구조분해 할당 : 배열 또는 객체에서 값을 분해
+const {name, age} = obj1; // 오른쪽 객체내 왼쪽에 각 변수/상수에 값 대입
+console.log(name)
+console.log(age)
+const [name2, ...array2] = obj2; // 오른쪽 배열내 순서대로 값들을 변수에 대입 , ...나머지들
+console.log(name2)
+console.log(array2) // 나머지
+
+// 9. 콜백함수 : 함수 전달 해서 나중에 함수 실행
+function printSuccess(message){console.log('성공',message);}
+function printScore(score, onSuccess, onError){
+    if (score >= 80) {onSuccess('합격')}
+    else {onError('불합격')}
+}
+// 콜백함수 방식으로 함수 호출 , 주의 : 인수에 함수 전달시 함수실행X
+printScore(50, printSuccess, (message)=>{console.log('실패'+message)})
+
+// 10. 동기식 : 먼저 호출한 함수/기능 결과가 올때까지 대기상태
+//     비동기 : 먼저 호출한 함수/기능 결과 순서 상관없이 반환
+// axios는 비동기 통신, 동기화로 만드는 방법 , (1) 함수앞에 async (2) axios 앞에 await
+const backLoad = async() => {
+    const response = await axios();
+}
+backLoad();
+
