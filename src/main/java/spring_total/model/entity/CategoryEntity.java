126edf12b397689bec71224cf6a8c25c6e3a40a2
seokam36 <seokam36@gmail.com>
2026-09-14 15:27:58 +0900

    total

--- src/main/java/spring_total/model/entity/CategoryEntity.java
+++ src/main/java/spring_total/model/entity/CategoryEntity.java
@@ -1,4 +1,35 @@
 package spring_total.model.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table (name = "category")
+@Getter @Setter @ToString @Builder @NoArgsConstructor @AllArgsConstructor
 public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer no;
+
+    private String name;
+
+    @OneToMany (mappedBy = "productEntity", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @Builder.Default
+    @ToString.Exclude
+    private List<ProductEntity> productEntities = new ArrayList<>();
 }
--- src/main/java/spring_total/model/entity/ReviewEntity.java
+++ src/main/java/spring_total/model/entity/ReviewEntity.java
@@ -1,4 +1,39 @@
 package spring_total.model.entity;
 
-public class ReviewEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity @Table(name = "review" )
+@NoArgsConstructor @AllArgsConstructor @Builder
+@Getter @Setter
+public class ReviewEntity{
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer rno;
+
+    @Column
+    private String reviewer;
+
+    @Column
+    private String content;
+
+    @Column
+    private Integer rating;
+
+    @ManyToOne
+    @JoinColumn( name = "b_no")
+    private ProductEntity productEntity;
+
+
 }
